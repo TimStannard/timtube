d76809385a19672c669224fea5c36019050d2dcd
Tim Stannard <stannard.tim@gmail.com>
2023-04-13 16:30:36 +1200

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
 
 function App() {
 
   return (
-    <div className="App">
-      Test
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/watch/:id" element={<Watch />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
